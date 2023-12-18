32f0be6b34df05d316115f3c6fa707e64b321b5c
2004-russell <151689516+2004-russell@users.noreply.github.com>
2023-12-18 09:04:03 +0000

    Add files via upload

---
+++ patient.js
@@ -0,0 +1,13 @@
+{
+    "surname": "Doe",
+    "otherNames": "John",
+    "gender": "Male",
+    "phoneNumber": "1234567890",
+    "residentialAddress": "123 Main St, Accra",
+    "emergencyContact": {
+      "name": "Emergency Contact Name",
+      "contact": "9876543210",
+      "relationship": "Friend"
+    }
+  }
+  
